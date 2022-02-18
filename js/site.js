33b52ca4215cd5b15d853d99f0a3481b655e0942
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-02-17 20:48:53 -0500

    finish

--- js/site.js
+++ js/site.js
@@ -42,13 +42,44 @@ function fizzBuzz(fizzValue, buzzValue)
         }
 
     }
-    returnArray;
+     return returnArray;
 
 }
 
 function displayData(fbArray){
 
-    //loop over the array and create a tablerow for each item
+    //get the table body element from the page
+    let tableBody = document.getElementById("results");
 
+    // get the template row
+    let templateRow = document.getElementById("fbTemplate")
+
+    //clear table first
+    tableBody.innerHTML = "";
+    
+    for (let i = 0; i < fbArray.length; i += 5) {
+
+        let tableRow = document.importNode(templateRow.content, true);
+
+        //grab just the tds
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].classList.add(fbArray[i]);
+        rowCols[0].textContent = fbArray[i];
+
+        rowCols[1].textContent = fbArray[i+1];
+        rowCols[1].classList.add(fbArray[i+1]);
+
+        rowCols[2].textContent = fbArray[i+2];
+        rowCols[2].classList.add(fbArray[i+2]);
+
+        rowCols[3].textContent = fbArray[i+3];
+        rowCols[3].classList.add(fbArray[i+3]);
+
+        rowCols[4].textContent = fbArray[i+4];
+        rowCols[4].classList.add(fbArray[i+4]);
+
+        tableBody.appendChild(tableRow);
+
+    }
     //add all the rows to the table
 }
